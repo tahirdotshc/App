6e7f6759e6b3413c9c9bacbd89f9b071a9c2df41
tahir <tahir.shc@gmail.com>
2025-08-11 20:39:42 +0500

    mylist added half added again

--- server/src/controllers/myList.controller.js
+++ server/src/controllers/myList.controller.js
@@ -1,6 +1,6 @@
 import myListModel from "../models/myList.modal";
 
-export const addToMyListController = async (requestAnimationFrame, response) {
+export const addToMyListController = async (requestAnimationFrame, response) => {
     try {
         const userId = requestAnimationFrame.userId
         const {productId, productTitle, image, rating, price, oldPrice, brand, discount } = request.body;
@@ -38,6 +38,44 @@ export const addToMyListController = async (requestAnimationFrame, response) {
 
     } catch (error) {
           return response.status(500).json({
+            message: error.message || error,
+            error: true,
+            success: false
+        })
+    }
+}
+
+export const deleteToMyListController = async (requestAnimationFrame, response) => {
+    try {
+        
+        const myListItem = await myListModel.findById(request.params.id);
+
+        if(!myListItem) {
+            return response.status(404).json({
+            message: "The item with this given id was not found",
+            error: true,
+            success: false
+        })
+        }
+
+        const deletedItem = await myListItem.findByIdAndDelete(request.params.id);
+
+        if(!deletedItem){
+            return response.status(404).json({
+            message: "The item is not deleted",
+            error: true,
+            success: false
+        }) 
+        }
+
+        return response.status(200).json({
+            message: "The item removed from my list",
+            error: false,
+            success: true
+        })
+
+    } catch (error) {
+           return response.status(500).json({
             message: error.message || error,
             error: true,
             success: false
--- server/src/routes/myList.route.js
+++ server/src/routes/myList.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import auth from "../middlewares/auth.js";
-import { addToMyListController } from "../controllers/myList.controller.js";
+import { addToMyListController, deleteToMyListController } from "../controllers/myList.controller.js";
 
 const myListRouter = Router();
 
 myListRouter.post("/add", auth, addToMyListController);
+myListRouter.delete("/:id", auth, deleteToMyListController);
 
 export default myListRouter;
