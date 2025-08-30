11c36e05f99339024b7abe4fbb5121a9e88e1b07
tahir <tahir.shc@gmail.com>
2025-08-30 21:59:30 +0500

    Lec 5 : 15:42

---
+++ src/components/AdsBannerSlider/index.jsx
@@ -0,0 +1,16 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Link } from 'react-router-dom'
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+import { Navigation } from 'swiper/modules';
+
+const AdsBannerSlider = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default AdsBannerSlider;
