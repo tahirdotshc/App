681a6672539914631250c446ddd81da76d3f2985
tahir <tahir.shc@gmail.com>
2025-09-08 19:12:19 +0500

    lec 5 - 39:20

---
+++ src/components/ProductsSlider/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ const ProductsSlider = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default ProductsSlider;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,14 +3,58 @@ import HomeSlider from '../../components/HomeSlider';
 import HomeCatSlider from '../../components/HomeCatSlider';
 import { LiaShippingFastSolid } from "react-icons/lia";
 import AdsBannerSlider from '../../components/AdsBannerSlider';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import ProductsSlider from '../../components/ProductsSlider';
+
 
 
 const Home = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <>
     <HomeSlider/>
     <HomeCatSlider/>
-    <br/><br/><br/><br/><br/><br/><br/>
+    
+    <section className="bg-white py-8">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          <div className="leftSec">
+            <h2 className="text-[20px] font-[600]">Popular Products</h2>
+            <p className="text-[10px] font-[500]">Do not miss the current affairs until the end of March.</p>
+          </div>
+
+          <div className="rightSec w-[60%]">
+            <Box sx={{ maxWidth: { xs: 320, sm: 480 }, bgcolor: 'background.paper' }}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+      >
+        <Tab label="Fashion" />
+        <Tab label="Electronics" />
+        <Tab label="Bags" />
+        <Tab label="Footwear" />
+        <Tab label="Groceries" />
+        <Tab label="Beauty" />
+        <Tab label="Wellness" />
+          <Tab label="Jewellery" />
+      </Tabs>
+    </Box>
+          </div>
+        </div>
+
+        <ProductsSlider/>
+      </div>
+    </section>
+
     <section className="py-16 bg-white">
       <div className="container">
         <div className="freeShipping w-full py-3 p-4 border-2 border-[#ff5252] flex items-center justify-between rounded-md mb-7">
